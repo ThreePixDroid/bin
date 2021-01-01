85005a9ffb7ec8caf1abfa1c88e2459f914d0b18
ThreePixDroid <ThreePixDroid@gmail.com>
2021-01-01 20:24:37 +0300

    new

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-import {Tool} from './tool.js'
+import { Tool } from './tool.js'
+import { Layer } from './layer.js'
+
+class App {
+  constructor(container) {
+    this.layer = new Layer(container);
+    this.tool = new Tool();
+  }
+
+  refresh() {
+    
+  }
+
+  display() {
+
+  }
+
+  animate() {
+
+  }
+}
+
+onload = () => {
+  new App(document.querySelector(`body`));
+}

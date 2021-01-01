d9daaa3e382faf212ece5c33d7546398cd4dead4
ThreePixDroid <ThreePixDroid@gmail.com>
2021-01-01 23:23:31 +0300

    preparing

---
+++ canvas/app.js
@@ -0,0 +1,28 @@
+class App {
+  constructor(container) {
+    this.layer = new Layer(container);
+  }
+}
+
+class Layer {
+  constructor(container) {
+    //create new Canvas element
+    this.canvas = document.createElement(`canvas`);
+    //get access to 2d drawing tools
+    this.context = this.canvas.getContext(`2d`);
+    //put Canvas to Container
+    container.appendChild(this.canvas);
+
+    this.fitToContainer(this.canvas);
+    addEventListener(`resize`, () => this.fitToContainer(this.canvas));
+  }
+  //fit Canvas size to container
+  fitToContainer(c) {
+    c.width = c.offsetWidth;
+    c.height = c.offsetHeight;
+  }
+}
+
+onload = () => {
+  new App(document.querySelector(`body`));
+}
